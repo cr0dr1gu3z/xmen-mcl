db009a32089508200c79be93ef05bafe3b7a3931
cr0dr1gu3z. <tawua4263@gmail.com>
2022-07-11 10:49:01 -0500

    optimizacion de coverage

--- src/main/java/com/xmen/app/mutantes/model/service/MutanteServiceImpl.java
+++ src/main/java/com/xmen/app/mutantes/model/service/MutanteServiceImpl.java
@@ -1,6 +1,5 @@
 package com.xmen.app.mutantes.model.service;
 
-
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
@@ -20,7 +19,6 @@ import com.xmen.app.mutantes.model.entity.NoMutante;
 import com.xmen.app.mutantes.model.request.RequestMutante;
 import com.xmen.app.mutantes.model.response.ResponseEstadistica;
 
-
 @Service
 public class MutanteServiceImpl implements IMutanteService {
 	@Autowired
@@ -32,31 +30,20 @@ public class MutanteServiceImpl implements IMutanteService {
 	@Autowired
 	private IBitacoraMutanteDao bitacoraMutanteDao;
 
+	private Integer contadorNomutante;
+	private Integer contadormutante;
+
 	@Override
 	@Transactional()
 	public ResponseEntity<Boolean> validaMutante(RequestMutante adn) {
-		Integer contadorNomutante = 0;
-		Integer contadormutante = 0;
+
 		if (Objects.isNull(adn)) {
 			return ResponseEntity.status(HttpStatus.FORBIDDEN).body(false);
 		} else {
-			if (adn.getDna() != null) {
-				for (String adnStr : adn.getDna()) {
-					Mutante mutante = mutanteDao.findByAdn(adnStr);
-					NoMutante noMutante = noMutanteDao.findByAdn(adnStr);
-					if (Objects.isNull(mutante))
-					{
-							
-						if (Objects.isNull(noMutante)) {
-					 	  return ResponseEntity.status(HttpStatus.FORBIDDEN).body(false);
-					 	} else {
-							contadorNomutante = contadorNomutante + 1;
-						}
-					} else {
-						contadormutante = contadormutante + 1;
-					}
-				}
-				if (contadormutante > 2) {
+			if (adn.getDna() != null) 
+			{
+				   recorreLista(adn);
+				if (contadormutante >= 2) {
 					persisteMutante(adn, true);
 					return ResponseEntity.status(HttpStatus.OK).body(true);
 				} else {
@@ -67,7 +54,21 @@ public class MutanteServiceImpl implements IMutanteService {
 				return ResponseEntity.status(HttpStatus.FORBIDDEN).body(false);
 			}
 		}
+	}
 
+	public void recorreLista(RequestMutante adn) {
+		contadorNomutante = 0;
+		contadormutante=0;
+		for (String adnStr : adn.getDna()) {
+			Mutante mutante = mutanteDao.findByAdn(adnStr);
+			if (Objects.isNull(mutante)) {
+				NoMutante noMutante = noMutanteDao.findByAdn(adnStr);
+				if (!Objects.isNull(noMutante))
+					contadorNomutante = contadorNomutante + 1;				
+			} else {
+				contadormutante = contadormutante + 1;
+			}
+		}
 	}
 
 	@Transactional
@@ -82,17 +83,15 @@ public class MutanteServiceImpl implements IMutanteService {
 				sb.append(adn);
 
 			}
-			
-			
-			 if(Objects.isNull(bitacoraMutanteDao.findByDna(sb.toString())))
-			 {
-				 bitacora.setDna(sb.toString());
-					bitacora.setCreateAt(date);
-					bitacora.setEsMutante(esMutante);
 
-					bitacoraMutanteDao.save(bitacora); 
-			 } 
-				
+			if (Objects.isNull(bitacoraMutanteDao.findByDna(sb.toString()))) {
+				bitacora.setDna(sb.toString());
+				bitacora.setCreateAt(date);
+				bitacora.setEsMutante(esMutante);
+
+				bitacoraMutanteDao.save(bitacora);
+			}
+
 		} catch (Exception e) {
 			e.getStackTrace();
 		}
@@ -104,23 +103,21 @@ public class MutanteServiceImpl implements IMutanteService {
 	public List<BitacoraValidacionMutante> findAll() {
 		return (List<BitacoraValidacionMutante>) bitacoraMutanteDao.findAll();
 	}
-	
 
 	@Override
 	@Transactional(readOnly = true)
-	public ResponseEstadistica estadisticas()
-	{
-		
+	public ResponseEstadistica estadisticas() {
+
 		ResponseEstadistica response = new ResponseEstadistica();
-		
-		if(!Objects.isNull(bitacoraMutanteDao.countMutant(false)) && !Objects.isNull(bitacoraMutanteDao.countMutant(true))) 
-		{
-				response.setCountMutantDNA(bitacoraMutanteDao.countMutant(true));
-				response.setCountHumanDNA(bitacoraMutanteDao.countMutant(false));				
-				response.setRatio((bitacoraMutanteDao.countMutant(false)==0)?0:(bitacoraMutanteDao.countMutant(true)/bitacoraMutanteDao.countMutant(false)));
-		}		
+
+		if (!Objects.isNull(bitacoraMutanteDao.countMutant(false))
+				&& !Objects.isNull(bitacoraMutanteDao.countMutant(true))) {
+			response.setCountMutantDNA(bitacoraMutanteDao.countMutant(true));
+			response.setCountHumanDNA(bitacoraMutanteDao.countMutant(false));
+			response.setRatio((bitacoraMutanteDao.countMutant(false) == 0) ? 0
+					: (bitacoraMutanteDao.countMutant(true) / bitacoraMutanteDao.countMutant(false)));
+		}
 		return response;
 	}
-	
 
 }
